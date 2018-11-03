6c39e1f22a3de35c41b598178be550a17144f2aa
JayZH <lyzhj122@gmail.com>
2018-11-03 01:41:00 -0400

    Jessica change the value of mystr

--- interactive widgets.py
+++ interactive widgets.py
@@ -31,5 +31,7 @@ phase = widgets.FloatSlider(min=0, max=5, value=0, description='Phase:')
 freq = widgets.FloatSlider(min=1, max=10, value=1, description='Freq:')
 
 mystr = 'Test'
+#Jessica make this
+mystr = 'Jessica'
 
 widgets.interactive(update_plot, amp=amp, phase=phase, freq=freq)
